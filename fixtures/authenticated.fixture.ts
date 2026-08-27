f3b7362f4d0fdefa8f33a187c569fa44bb97da9e
gabrielsarriavillada <gabrielsarriavillada@gmail.com>
2026-08-27 11:59:24 +0200

    Add missing semicolon

--- fixtures/authenticated.fixture.ts
+++ fixtures/authenticated.fixture.ts
@@ -1,6 +1,6 @@
 import type { Page } from "@playwright/test";
 import { test as apiTest } from "./api.fixture.js";
-import type { UserDetails } from "../api/models/userDetails.js"
+import type { UserDetails } from "../api/models/userDetails.js";
 import { createUserDetails } from "../api/helpers/createUserDetails.js";
 import { authenticateUser } from "../api/helpers/authenticateUser.js";
 import { createAuthStorageState } from "../api/helpers/createAuthStorageState.js";
@@ -8,7 +8,7 @@ import { createAuthStorageState } from "../api/helpers/createAuthStorageState.js
 type AuthenticatedFixture = {
     userDetails: UserDetails;
     authenticatedPage: Page;
-}
+};
 
 export const test = apiTest.extend<AuthenticatedFixture>({
     userDetails: async({}, use) => {
