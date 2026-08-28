434cc9b6552c6ad7eed0c92effc9b96ec6d0a2d8
gabrielsarriavillada <gabrielsarriavillada@gmail.com>
2026-08-28 11:36:59 +0200

    Add favorites and products classes to the custom api fixture

--- fixtures/api.fixture.ts
+++ fixtures/api.fixture.ts
@@ -1,11 +1,15 @@
 import { request as playwrightRequest, test as base } from "@playwright/test";
 import { BrandsClient } from "../api/clients/BrandsClient.js";
 import { UsersClient } from "../api/clients/UsersClient.js";
+import { FavoritesClient } from "../api/clients/FavoritesClient.js";
+import { ProductsClient } from "../api/clients/ProductsClient.js";
 import { env } from "../config/env.js";
 
 type ApiFixtures = {
     brandsClient: BrandsClient;
     usersClient: UsersClient;
+    favoritesClient: FavoritesClient;
+    productsClient: ProductsClient;
 };
 
 export const test = base.extend<ApiFixtures>({
@@ -25,6 +29,25 @@ export const test = base.extend<ApiFixtures>({
         });
 
         await use(new BrandsClient(apiContext));
+
+        await apiContext.dispose();
+    },
+
+    favoritesClient: async ({}, use) => {
+        const apiContext = await playwrightRequest.newContext({
+            baseURL: env.apiBaseUrl,
+        });
+
+        await use(new FavoritesClient(apiContext));
+
+        await apiContext.dispose();
+    },
+    productsClient: async({}, use) => {
+        const apiContext = await playwrightRequest.newContext({
+            baseURL: env.apiBaseUrl,
+        });
+
+        await use(new ProductsClient(apiContext));
 
         await apiContext.dispose();
     },
